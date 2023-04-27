3055c1e38aa32788431410dc5df249b561e87dcf
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-27 16:16:52 -0600

    Initial Commit

---
+++ SimplePhotos/SimplePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosApp.swift
+//  SimplePhotos
+//
+//  Created by Brenda Saavedra  on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
