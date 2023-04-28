042b9f9922ad85a5a6d3c21752a460e762f3b272
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-28 10:24:11 -0600

    feat: add NetworkReachability to see if network is available

---
+++ SimplePhotos/Utils/NetworkReachability.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkReachability.swift
+//  SimplePhotos
+//
+//  Created by Brenda Saavedra  on 28/04/23.
+//
+
+import SystemConfiguration
+
+class NetworkReachability: ObservableObject {
+    @Published var reachable: Bool = false
+    private let reachability = SCNetworkReachabilityCreateWithName(nil, "www.google.com")
+    
+    init() {
+        self.reachable = checkConnection()
+    }
+    
+    func checkConnection() -> Bool {
+        var flags = SCNetworkReachabilityFlags()
+        SCNetworkReachabilityGetFlags(reachability!, &flags)
+     
+        return isNetworkReachable(with: flags)
+    }
+    
+    private func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+        let isReachable = flags.contains(.reachable)
+        let connectionRequired = flags.contains(.connectionRequired)
+        let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        let canConnectWithoutIntervention = canConnectAutomatically && !flags.contains(.interventionRequired)
+        return isReachable && (!connectionRequired || canConnectWithoutIntervention)
+    }
+}
